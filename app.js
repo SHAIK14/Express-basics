8570170678fab5eb5c81c9012cbfb668856507ba
shaik Asif <smd.20sa@gmail.com>
2023-08-03 21:43:41 +0530

    done with html

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+const path = require("path");
+
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.status(404).send("<p>page not found</P>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-app.listen(4000);
+app.listen(3000);
